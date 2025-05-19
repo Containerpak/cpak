cbfea066794cf4d5f8d7391821cf43604d84ae21
mirkobrombin <brombin@mirko.pm>
2025-05-19 10:57:44 +0200

    feat: update Go module version and refactor override command using github.com/mirkobrombin/go-struct-flags/v1/binder

--- go.mod
+++ go.mod
@@ -1,16 +1,15 @@
 module github.com/mirkobrombin/cpak
 
-go 1.21
+go 1.23.4
 
 require (
 	github.com/creack/pty v1.1.21
 	github.com/distrobox/hrun v0.0.2
-	github.com/gdamore/tcell/v2 v2.8.1
 	github.com/google/go-containerregistry v0.16.1
 	github.com/google/uuid v1.3.1
 	github.com/mirkobrombin/dabadee v0.4.0
+	github.com/mirkobrombin/go-struct-flags v1.0.1
 	github.com/olekukonko/tablewriter v0.0.5
-	github.com/rivo/tview v0.0.0-20250501113434-0c592cd31026
 	github.com/schollz/progressbar/v3 v3.13.1
 	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/spf13/cobra v1.8.0
@@ -24,13 +23,12 @@ require (
 	github.com/docker/distribution v2.8.2+incompatible // indirect
 	github.com/docker/docker v25.0.6+incompatible // indirect
 	github.com/docker/docker-credential-helpers v0.7.0 // indirect
-	github.com/gdamore/encoding v1.0.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.16.5 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/minio/highwayhash v1.0.2 // indirect
--- pkg/types/override.go
+++ pkg/types/override.go
@@ -1,35 +1,35 @@
 package types
 
 type Override struct {
-	SocketX11        bool `json:"socketX11"`
-	SocketWayland    bool `json:"socketWayland"`
-	SocketPulseAudio bool `json:"socketPulseAudio"`
-	SocketSessionBus bool `json:"socketSessionBus"`
-	SocketSystemBus  bool `json:"socketSystemBus"`
-	SocketSshAgent   bool `json:"socketSshAgent"`
-	SocketCups       bool `json:"socketCups"`
-	SocketGpgAgent   bool `json:"socketGpgAgent"`
-	SocketAtSpiBus   bool `json:"socketAtSpiBus"`
+	SocketX11        bool `json:"socketX11" flag:"socketX11,bool"`
+	SocketWayland    bool `json:"socketWayland" flag:"socketWayland,bool"`
+	SocketPulseAudio bool `json:"socketPulseAudio" flag:"socketPulseAudio,bool"`
+	SocketSessionBus bool `json:"socketSessionBus" flag:"socketSessionBus,bool"`
+	SocketSystemBus  bool `json:"socketSystemBus" flag:"socketSystemBus,bool"`
+	SocketSshAgent   bool `json:"socketSshAgent" flag:"socketSshAgent,bool"`
+	SocketCups       bool `json:"socketCups" flag:"socketCups,bool"`
+	SocketGpgAgent   bool `json:"socketGpgAgent" flag:"socketGpgAgent,bool"`
+	SocketAtSpiBus   bool `json:"socketAtSpiBus" flag:"socketAtSpiBus,bool"`
 
-	DeviceDri bool `json:"deviceDri"`
-	DeviceKvm bool `json:"deviceKvm"`
-	DeviceShm bool `json:"deviceShm"`
-	DeviceAll bool `json:"deviceAll"`
+	DeviceDri bool `json:"deviceDri" flag:"deviceDri,bool"`
+	DeviceKvm bool `json:"deviceKvm" flag:"deviceKvm,bool"`
+	DeviceShm bool `json:"deviceShm" flag:"deviceShm,bool"`
+	DeviceAll bool `json:"deviceAll" flag:"deviceAll,bool"`
 
-	Notification bool `json:"notification"`
+	Notification bool `json:"notification" flag:"notification,bool"`
 
-	FsHost     bool     `json:"fsHost"`
-	FsHostEtc  bool     `json:"fsHostEtc"`
-	FsHostHome bool     `json:"fsHostHome"`
-	FsExtra    []string `json:"fsExtra"`
+	FsHost     bool     `json:"fsHost" flag:"fsHost,bool"`
+	FsHostEtc  bool     `json:"fsHostEtc" flag:"fsHostEtc,bool"`
+	FsHostHome bool     `json:"fsHostHome" flag:"fsHostHome,bool"`
+	FsExtra    []string `json:"fsExtra" flag:"fsExtra,strings"`
 
-	Env     []string `json:"env"`
-	Network bool     `json:"network"`
-	Process bool     `json:"process"`
+	Env     []string `json:"env" flag:"env,strings"`
+	Network bool     `json:"network" flag:"network,bool"`
+	Process bool     `json:"process" flag:"process,bool"`
 
-	AsRoot bool `json:"asRoot"`
+	AsRoot bool `json:"asRoot" flag:"asRoot,bool"`
 
-	AllowedHostCommands []string `json:"allowedHostCommands"`
+	AllowedHostCommands []string `json:"allowedHostCommands" flag:"allowedHostCommands,strings"`
 }
 
 func NewOverride() Override {
