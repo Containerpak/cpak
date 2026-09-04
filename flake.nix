4ab4b4fbd125210c8d89a31ff1daf6ec65ae6ac9
mirkobrombin <brombin94@gmail.com>
2026-09-04 14:37:41 +0200

    chore: prepare 2.12.6

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs }:
     let
-      version = "2.12.5";
+      version = "2.12.6";
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
